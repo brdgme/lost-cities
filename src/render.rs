cf887f225b46168f6159cc6a652f2c1e41aa0235
Michael Alexander <beefsack@gmail.com>
2016-08-30 16:07:38 +1000

    Player checking in render function

--- src/render.rs
+++ src/render.rs
@@ -1,9 +1,17 @@
 use brdgme_game::{Renderer, GameError};
-use brdgme_markup::ast::Node;
+use brdgme_markup::ast::{Node as N, Align as A};
 use super::Game;
 
 impl Renderer for Game {
-    fn render(&self, _player: Option<usize>) -> Result<Vec<Node>, GameError> {
-        Err(GameError::Internal("not implemented".to_string()))
+    fn render(&self, player: Option<usize>) -> Result<Vec<N>, GameError> {
+        let persp = player.unwrap_or(0);
+        if persp > 1 {
+            return Err(GameError::Internal("invalid player number".to_string()));
+        }
+        Ok(vec![N::Align(A::Center,
+                         80,
+                         vec![
+            N::Text("lost cities!".to_string()),
+        ])])
     }
 }
