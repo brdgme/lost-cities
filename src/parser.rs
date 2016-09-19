32a4016c37f52470c0c749c319198d35ec7006d7
Michael Alexander <beefsack@gmail.com>
2016-09-19 11:27:39 +1000

    Updated dependencies and case insensitive command names

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,11 @@
-use combine::{choice, token, Parser, ParserExt, many1, parser, try};
-use combine::char::{digit, string, spaces};
+use combine::{choice, token, Parser, many1, parser, try};
+use combine::char::{digit, spaces, string_cmp};
 use combine::primitives::{Stream, ParseResult};
 use combine::combinator::FnParser;
 
 use card::{Expedition, Value, Card};
+
+use std::ascii::AsciiExt;
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Command {
@@ -15,13 +17,17 @@ pub enum Command {
 
 type FnP<T, I> = FnParser<I, fn(I) -> ParseResult<T, I>>;
 
+fn cmp_ignore_case(l: char, r: char) -> bool {
+    l.eq_ignore_ascii_case(&r)
+}
+
 pub fn command<I>() -> FnP<Command, I>
     where I: Stream<Item = char>
 {
     fn command_<I>(input: I) -> ParseResult<Command, I>
         where I: Stream<Item = char>
     {
-        choice([play, discard, take, draw]).parse_state(input)
+        choice([play, discard, take, draw]).parse_stream(input)
     }
     parser(command_)
 }
@@ -29,35 +35,35 @@ pub fn command<I>() -> FnP<Command, I>
 fn play<I>(input: I) -> ParseResult<Command, I>
     where I: Stream<Item = char>
 {
-    (try(string("play")), spaces(), parser(card))
+    (try(string_cmp("play", cmp_ignore_case)), spaces(), parser(card))
         .map(|(_, _, c)| Command::Play(c))
-        .parse_state(input)
+        .parse_stream(input)
 }
 
 fn discard<I>(input: I) -> ParseResult<Command, I>
     where I: Stream<Item = char>
 {
-    (try(string("discard")), spaces(), parser(card))
+    (try(string_cmp("discard", cmp_ignore_case)), spaces(), parser(card))
         .map(|(_, _, c)| Command::Discard(c))
-        .parse_state(input)
+        .parse_stream(input)
 }
 
 fn take<I>(input: I) -> ParseResult<Command, I>
     where I: Stream<Item = char>
 {
-    (try(string("take")),
+    (try(string_cmp("take", cmp_ignore_case)),
      spaces(),
      parser(expedition).message("expected expedition, eg. 'w' or 'y'"))
         .map(|(_, _, e)| Command::Take(e))
-        .parse_state(input)
+        .parse_stream(input)
 }
 
 fn draw<I>(input: I) -> ParseResult<Command, I>
     where I: Stream<Item = char>
 {
-    try(string("draw"))
+    try(string_cmp("draw", cmp_ignore_case))
         .map(|_| Command::Draw)
-        .parse_state(input)
+        .parse_stream(input)
 }
 
 fn card<I>(input: I) -> ParseResult<Card, I>
@@ -65,62 +71,62 @@ fn card<I>(input: I) -> ParseResult<Card, I>
 {
     (parser(expedition), parser(value))
         .message("expected card, eg. 'wx' or 'r7'")
-        .parse_state(input)
+        .parse_stream(input)
 }
 
 fn expedition<I>(input: I) -> ParseResult<Expedition, I>
     where I: Stream<Item = char>
 {
     choice([expedition_red, expedition_green, expedition_white, expedition_blue, expedition_yellow])
-        .parse_state(input)
+        .parse_stream(input)
 }
 
 fn expedition_red<I>(input: I) -> ParseResult<Expedition, I>
     where I: Stream<Item = char>
 {
-    choice([token('r'), token('R')]).map(|_| Expedition::Red).parse_state(input)
+    choice([token('r'), token('R')]).map(|_| Expedition::Red).parse_stream(input)
 }
 
 fn expedition_green<I>(input: I) -> ParseResult<Expedition, I>
     where I: Stream<Item = char>
 {
-    choice([token('g'), token('G')]).map(|_| Expedition::Green).parse_state(input)
+    choice([token('g'), token('G')]).map(|_| Expedition::Green).parse_stream(input)
 }
 
 fn expedition_white<I>(input: I) -> ParseResult<Expedition, I>
     where I: Stream<Item = char>
 {
-    choice([token('w'), token('W')]).map(|_| Expedition::White).parse_state(input)
+    choice([token('w'), token('W')]).map(|_| Expedition::White).parse_stream(input)
 }
 
 fn expedition_blue<I>(input: I) -> ParseResult<Expedition, I>
     where I: Stream<Item = char>
 {
-    choice([token('b'), token('B')]).map(|_| Expedition::Blue).parse_state(input)
+    choice([token('b'), token('B')]).map(|_| Expedition::Blue).parse_stream(input)
 }
 
 fn expedition_yellow<I>(input: I) -> ParseResult<Expedition, I>
     where I: Stream<Item = char>
 {
-    choice([token('y'), token('Y')]).map(|_| Expedition::Yellow).parse_state(input)
+    choice([token('y'), token('Y')]).map(|_| Expedition::Yellow).parse_stream(input)
 }
 
 fn value<I>(input: I) -> ParseResult<Value, I>
     where I: Stream<Item = char>
 {
-    choice([value_investment, value_n]).parse_state(input)
+    choice([value_investment, value_n]).parse_stream(input)
 }
 
 fn value_investment<I>(input: I) -> ParseResult<Value, I>
     where I: Stream<Item = char>
 {
-    choice([token('x'), token('X')]).map(|_| Value::Investment).parse_state(input)
+    choice([token('x'), token('X')]).map(|_| Value::Investment).parse_stream(input)
 }
 
 fn value_n<I>(input: I) -> ParseResult<Value, I>
     where I: Stream<Item = char>
 {
-    many1(digit()).map(|d: String| Value::N(d.parse::<usize>().unwrap())).parse_state(input)
+    many1(digit()).map(|d: String| Value::N(d.parse::<usize>().unwrap())).parse_stream(input)
 }
 
 #[cfg(test)]
