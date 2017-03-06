f628a7070acb5a90c70d3367dbf72b7c2dc3f486
Michael Alexander <beefsack@gmail.com>
2017-03-07 10:35:54 +1100

    Fixed error since updating game error helper

--- src/lib.rs
+++ src/lib.rs
@@ -449,7 +449,7 @@ impl Gamer for Game {
                 self.take(player, e).map(|l| (l, remaining.to_string()))
             }
             Ok((Command::Draw, remaining)) => self.draw(player).map(|l| (l, remaining.to_string())),
-            Err(e) => Err(brdgme_game::parser::to_game_error(e)),
+            Err(e) => Err(brdgme_game::parser::to_game_error(&e)),
         }
     }
 }
