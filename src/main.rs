d2ede9ebf82e9fabb59c2fc58d1d5c4aac7c14b5
Michael Alexander <beefsack@gmail.com>
2017-03-14 20:10:10 +1100

    Added executable

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate brdgme_cmd;
+
+extern crate lost_cities;
+
+use brdgme_cmd::cli;
+use lost_cities::Game;
+use std::io::{stdin, stdout};
+
+fn main() {
+    cli::<Game, _, _>(stdin(), &mut stdout());
+}
